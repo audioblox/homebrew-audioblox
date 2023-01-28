ae5efc311a623a67ae566a701e97f74fee2dd5cb
Bas Donker <bas.donker@forma.ai>
2023-01-28 09:59:26 -0500

    Update

--- abctl.rb
+++ abctl.rb
@@ -1,8 +1,8 @@
 class Abctl < Formula
     desc "A CLI tool for audioblox editors"
     homepage "https://github.com/audioblox/abctl"
-    url "https://github.com/audioblox/abctl/releases/download/v0.1.5/abctl.tar.gz"
-  sha256 "8a6aad43d5ce87d1241db48228a018d908a1d32fa6461e5c850fc1a0ff0ade74"
+    url "https://github.com/audioblox/abctl/releases/download/v0.1.6/abctl.tar.gz"
+    sha256 "07863aa24100474d75a1814ef9ddb35079289ba2599ff2f16b149cf5961c19af"
     license "MIT"
     
     def install
